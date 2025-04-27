02393c2f015d5b4ed91792d978dc306f25cd4efc
Grace <grace6424@icloud.com>
2025-04-27 11:41:31 +0200

    break

--- lib/src/main_screen.dart
+++ lib/src/main_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:jp_app/src/common/widgets/app_bg.dart';
 import 'package:jp_app/src/common/widgets/cup_cake_bg.dart';
@@ -11,7 +13,7 @@ class MainScreen extends StatelessWidget {
       clipBehavior: Clip.none,
       children: [
         AppBg(appBg: 'assets/bg/bg_startscreen.png'),
-        Center(child: CupCakeBg(snacOpacity: 0.3)),
+        Center(child: CupCakeBg(snacOpacity: 0.8)),
         Scaffold(
           backgroundColor: Colors.transparent,
           body: Padding(
@@ -20,132 +22,169 @@ class MainScreen extends StatelessWidget {
 
             ///////
             child: Column(
-              
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                
-                Expanded(
-                  flex: 2,
-                  child: Container(height: 50)),
-                Expanded(
-                  flex: 1,
-                  child: Container(height: 50)),
-                Container(
-                  clipBehavior: Clip.none,
-                  padding: const EdgeInsets.fromLTRB(5, 20, 5, 0),
-                  decoration: ShapeDecoration(
-                    color: const Color.fromARGB(62, 255, 255, 255),
-                
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(width: 1, color: Colors.white),
-                      borderRadius: BorderRadius.circular(35),
-                    ),
-                  ),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Feeling Snackish Today?',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontFamily: 'InterBlack',
-                          fontWeight: FontWeight.w900,
-                          height: 1.3,
-                          letterSpacing: 0.2,
-                          shadows: [
-                            Shadow(
-                              offset: Offset(0, 30),
-                              blurRadius: 180,
-                              color: Color(0xFF000000),
+                Expanded(flex: 5, child: Container(height: 50)),
+                // Expanded(
+                //   flex: 1,
+                //   child: Container(height: 50)),
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                    child: Container(
+                      clipBehavior: Clip.none,
+                      padding: const EdgeInsets.fromLTRB(5, 20, 5, 0),
+                      decoration: ShapeDecoration(
+                        color: const Color.fromARGB(12, 50, 50, 50),
+                    
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(width: 0.5, color: Color.fromARGB(97, 126, 130, 126)),
+                          borderRadius: BorderRadius.circular(35),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Feeling Snackish Today?',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontFamily: 'InterBlack',
+                              fontWeight: FontWeight.w900,
+                              height: 1.3,
+                              letterSpacing: 0.2,
+                              /////////////////////////////////////
+                              shadows: [
+                                Shadow(
+                                  offset: Offset(0, 30),
+                                  blurRadius: 180,
+                                  color: Color(0xFF000000),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(25.4, 8, 25.4, 0),
-                        child: Text(
-                          "Explore Angi's most popular snack selection and get instantly happy.",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Color(0x99EBEBF5),
-                            fontFamily: 'SFPro',
-                            fontWeight: FontWeight.w400,
-                            height: 1.3,
-                            letterSpacing: 1,
-                            shadows: [
-                              Shadow(
-                                offset: Offset(0, 30),
-                                blurRadius: 180,
-                                color: Color(0xFF000000),
-                              ),
-                            ],
                           ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 35, 0, 35),
-                        child: Stack(
-                          children: [
-                            Container(
-                              decoration: ShapeDecoration(
-                                gradient: LinearGradient(
-                                  begin: Alignment(0.5, 0),
-                                  end: Alignment(0.5, 1),
-                                  colors: [
-                                    const Color.fromARGB(28, 0, 0, 0),
-                                    const Color.fromARGB(28, 255, 255, 255),
-                                  ],
-                                ),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16),
-                                ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(25.4, 8, 25.4, 0),
+                            child: Text(
+                              "Explore Angi's most popular snack selection and get instantly happy.",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Color(0x99EBEBF5),
+                                fontFamily: 'SFPro',
+                                fontWeight: FontWeight.w400,
+                                height: 1.3,
+                                letterSpacing: 1,
+                                shadows: [
+                                  Shadow(
+                                    offset: Offset(0, 30),
+                                    blurRadius: 180,
+                                    color: Color(0xFF000000),
+                                  ),
+                                ],
                               ),
-                              height: 50,
-                              width: 230,
-                              child: TextButton(
-                                onPressed: () {},
-                                child: Text(
-                                  'Order Now',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Inter',
-                                    fontWeight: FontWeight.w600,
-                                    height: 1.3,
-                                    shadows: [
-                                      Shadow(
-                                        offset: Offset(0, 8),
-                                        blurRadius: 60,
-                                        color: Color.fromARGB(180, 80, 80, 80),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 35, 0, 35),
+                            child: Stack(
+                              children: [
+                    
+                    Opacity(
+                                  opacity: 1,
+                                  child: Container(
+                                    height: 50,
+                                    width: 230,
+                    
+                                    decoration: ShapeDecoration(
+                                      gradient: RadialGradient(
+                                        center: Alignment.bottomRight,
+                                        radius: 5,
+                    
+                                        colors: [
+                                          Color.fromARGB(255, 246, 158, 163),
+                                          Color.fromARGB(255, 233, 122, 196),
+                                        ],
                                       ),
-                                    ],
+                                      // color: Colors.amber,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(16),
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                            Opacity(
-                              opacity: 0.35,
-                              child: Container(
-                                height: 50,
-                                width: 230,
+                    
+                    
+                                Container(
+                                  decoration: ShapeDecoration(
+                                    shadows: [
+                                      
+                                      BoxShadow(
+                                        blurStyle: BlurStyle.inner,
+                                        offset: Offset(0, 2.5),
+                                        blurRadius: 60,
+                                        color: Color.fromARGB(120, 147, 117, 182),
+                                      ),
+                                      BoxShadow(
+                                        blurStyle: BlurStyle.inner,
+                                        offset: Offset(0, -2.5),
+                                        blurRadius: 70,
+                                        color: Color.fromARGB(100, 255, 172, 228),
+                                      ),
+                    
+                                    ],
+                                    gradient: LinearGradient(
+                                      begin: Alignment(0.5, 0),
+                                      end: Alignment(0.5, 1),
+                                      colors: [
+                                        Color.fromARGB(20, 254, 200, 241),
+                                        Color.fromARGB(20, 255, 255, 255),
+                    
+                                        //A28
+                                      ],
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                  ),
+                                  height: 50,
+                                  width: 230,
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Order Now',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'Inter',
+                                        fontWeight: FontWeight.w600,
+                                        height: 1.3,
+                                        shadows: [
+                                          Shadow(
+                                            offset: Offset(0, 8),
+                                            blurRadius: 60,
+                                            color: Color.fromARGB(180, 80, 80, 80),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                    
                                 
-                                decoration: ShapeDecoration(
-                                  color: Colors.amber,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(16)
-                
-                                  )),
-                              ),
+                    
+                                
+                                
+                               
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Container(height: 62)),
+                Expanded(flex: 1, child: Container(height: 62)),
               ],
             ),
           ),
